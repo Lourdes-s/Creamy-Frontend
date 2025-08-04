df48771275b44bdd1f7367ab9ef1d0365790d603
Lourdes.s <lou_santillan98@hotmail.com>
2025-08-04 16:33:33 -0300

    add responsive nav

--- src/Components/Common/Nav/Nav.jsx
+++ src/Components/Common/Nav/Nav.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { FaUser, FaShoppingCart, FaPlus } from 'react-icons/fa'
+import { FaUser, FaShoppingCart, FaPlus, FaBars, FaTimes } from 'react-icons/fa'
 import { AuthContext } from '../../../Context/AuthContext'
 import LogoCreamy from '../../../Assets/images/Logo.png'
 import './nav.css'
@@ -13,6 +13,8 @@ const Nav = () => {
 
     const [showDropdown, setShowDropdown] = useState(false)
     const dropdownRef = useRef(null)
+    
+    const [menuOpen, setMenuOpen] = useState(false)
 
     // Cierra el menú si se hace clic fuera
     useEffect(() => {
@@ -28,12 +30,16 @@ const Nav = () => {
 
     return (
         <nav className='navbar'>
-
             <div className='nav-left'>
-                <Link to="/home" className="logo"> <img src={LogoCreamy} alt="logo" /></Link>
+                <Link to="/home" className="logo">
+                    <img src={LogoCreamy} alt="logo" />
+                </Link>
 
-                <div className='dropdown' ref={dropdownRef}>
-                    <button className='dropdown-toggle' onClick={() => setShowDropdown(!showDropdown)}> Productos </button>
+                {/* Menú Desktop */}
+                <div className='dropdown desktop-only' ref={dropdownRef}>
+                    <button className='dropdown-toggle' onClick={() => setShowDropdown(!showDropdown)}> 
+                        Productos 
+                    </button>
                     <div className={`dropdown-menu ${showDropdown ? 'show' : ''}`}>
                         <Link to='/categoria/cuidado-capilar'>Cuidado capilar</Link>
                         <Link to='/categoria/cuidado-personal'>Cuidado personal</Link>
@@ -43,13 +49,11 @@ const Nav = () => {
                     </div>
                 </div>
 
-                <a href="/contact" className="nav-link">Contacto</a>
-
-                <a href="/about_us" className="nav-link">Sobre Creamy</a>
+                <a href="/contact" className="nav-link desktop-only">Contacto</a>
+                <a href="/about_us" className="nav-link desktop-only">Sobre Creamy</a>
             </div>
 
-            <div className='nav-right'>
-
+            <div className='nav-right desktop-only'>
                 {is_authenticated_state ? (
                     <div className='dropdown' ref={userMenuRef}>
                         <button className='dropdown-toggle' onClick={() => setShowUserMenu(!showUserMenu)}>
@@ -74,6 +78,63 @@ const Nav = () => {
                         <FaPlus />
                     </Link>
                 )}
+            </div>
+
+            {/* Botón Hamburguesa */}
+            <button 
+                className={`hamburger ${menuOpen ? 'active' : ''}`} 
+                onClick={() => setMenuOpen(!menuOpen)}
+                aria-label="Menú"
+            >
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+
+            {/* Menú Mobile */}
+            <div className={`mobile-menu ${menuOpen ? 'open' : ''}`}>
+                <div className='dropdown' ref={dropdownRef}>
+                    <button className='dropdown-toggle' onClick={() => setShowDropdown(!showDropdown)}> 
+                        Productos 
+                    </button>
+                    <div className={`dropdown-menu ${showDropdown ? 'show' : ''}`}>
+                        <Link to='/categoria/cuidado-capilar'>Cuidado capilar</Link>
+                        <Link to='/categoria/cuidado-personal'>Cuidado personal</Link>
+                        <Link to='/categoria/dermocosmetica'>Dermocosmética</Link>
+                        <Link to='/categoria/fragancias'>Fragancias</Link>
+                        <Link to='/categoria/maquillaje'>Maquillaje</Link>
+                    </div>
+                </div>
+
+                <a href="/contact" className="nav-link">Contacto</a>
+                <a href="/about_us" className="nav-link">Sobre Creamy</a>
+
+                <div className='mobile-icons'>
+                    {is_authenticated_state ? (
+                        <div className='dropdown' ref={userMenuRef}>
+                            <button className='dropdown-toggle' onClick={() => setShowUserMenu(!showUserMenu)}>
+                                <FaUser /> <span>{user?.name}</span>
+                            </button>
+                            <div className={`dropdown-menu ${showUserMenu ? 'show' : ''}`}>
+                                <button className='dropdown-item' onClick={() => { logout(); setShowUserMenu(false); setMenuOpen(false); }}>
+                                    Cerrar sesión
+                                </button>
+                            </div>
+                        </div>
+                    ) : (
+                        <Link to="/login" className="nav-link"><FaUser /></Link>
+                    )}
+
+                    <Link to="/carrito" className="icon-link">
+                        <FaShoppingCart />
+                    </Link>
+
+                    {isAdmin && (
+                        <Link to={'/product/new'} className="icon-link">
+                            <FaPlus />
+                        </Link>
+                    )}
+                </div>
             </div>
         </nav>
     )
